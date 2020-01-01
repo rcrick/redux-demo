9119696b0433651b195ba2ae7fec737ec8c7499e
Rick Xu <johndoe@example.com>
2020-01-01 13:00:36 +0800

    Init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,97 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red'
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
+  }
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function renderApp(newAppState, oldAppState = {}) {
+  if (newAppState === oldAppState) {
+    return;
+  }
+  console.log('render app...');
+  renderTitle(newAppState.title, oldAppState.title);
+  renderContent(newAppState.content, oldAppState.content);
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function renderTitle(newTitle, oldTitle = {}) {
+  if (newTitle === oldTitle) return; // 数据没有变化就不渲染了
+  console.log('render title...');
+  const titleDOM = document.getElementById('title');
+  titleDOM.innerHTML = newTitle.text;
+  titleDOM.style.color = newTitle.color;
+}
+
+function renderContent(newContent, oldContent = {}) {
+  if (newContent === oldContent) return; // 数据没有变化就不渲染了
+  console.log('render content...');
+  const contentDOM = document.getElementById('content');
+  contentDOM.innerHTML = newContent.text;
+  contentDOM.style.color = newContent.color;
+}
+
+function stateChanger(state, action) {
+  if (!state) {
+    return {
+      title: {
+        text: 'React.js 小书',
+        color: 'red'
+      },
+      content: {
+        text: 'React.js 小书内容',
+        color: 'blue'
+      }
+    };
+  }
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      };
+    case 'UPDATE_TITLE_COLOR':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      };
+    default:
+      return state;
+  }
+}
+function createStore(reducer) {
+  let state = null;
+  const listeners = [];
+  const subscribe = listener => listeners.push(listener);
+  const getState = () => state;
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listener => {
+      listener();
+    });
+  };
+  dispatch({});
+  return { getState, dispatch, subscribe };
+}
+
+const store = createStore(stateChanger);
+let oldState = store.getState();
+store.subscribe(() => {
+  const newState = store.getState();
+  renderApp(newState, oldState);
+  oldState = newState;
+});
+
+renderApp(store.getState()); // 首次渲染页面
+store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }); // 修改标题文本
+store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'red' }); // 修改标题颜色
+// renderApp(store.getState()); // 把新的数据渲染到页面上
